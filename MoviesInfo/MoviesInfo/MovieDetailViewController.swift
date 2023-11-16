8d342eba7b50e63d9e3cfa8911a7dff379323d4d
youjiin0325 <youjiin0325@gmail.com>
2023-11-16 17:43:51 +0900

    Creat: 메인테이블뷰와 디테일 뷰 생성

---
+++ MoviesInfo/MoviesInfo/MovieDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  MovieDetailViewController.swift
+//  MoviesInfo
+//
+//  Created by YOU on 11/16/23.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+//    @IBOutlet var webView: WKWebView!
+    
+    var webSite: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.largeTitleDisplayMode = .never
+        
+      
+        
+        
+    
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
